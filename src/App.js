88bd0b0ed09e012604eaa5a03abb8dbf5ee78043
sai1781 <96867450+sai1781@users.noreply.github.com>
2022-10-29 17:16:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAirbnb } from '@fortawesome/free-brands-svg-icons' 
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>The Evolution of Airbnb's Frontend</h1>
+      <p>@spikebrehm</p>
+      <FontAwesomeIcon fontSize={45} icon={faAirbnb} />
+      <p>airbnb</p>
+     
+
+
+    </div>
+
+    
+    
+  );
+}
+
+export default App;
